da92b1d5df5d99af15aa1fb746fe3ca5f147b8fa
alumno <alumno>
2016-02-10 10:02:23 +0100

    emailconfig

--- dao/contacto.php
+++ dao/contacto.php
@@ -12,8 +12,6 @@ $mail = new PHPMailer;
 $mail->CharSet="UTF-8";
 //Set who the message is to be sent from
 $mail->setFrom('danielhdz364@gmail.com');
-//Set an alternative reply-to address
-$mail->addReplyTo('danielhdz364@gmail.com');
 //Set who the message is to be sent to
 $mail->addAddress('danielhdz364@gmail.com');
 //Set the subject line
@@ -28,18 +26,16 @@ $mail->AltBody = 'This is a plain-text message body';
 if (!$mail->send()) {
     echo "Error al enviar el Email";
 } else {
-
     echo "Email Enviado!";
     $mensaje2='Buenas ' .$name. ' pronto el soporte se pondra en contacto contigo.';
     require_once('./PHPMailer/class.phpmailer.php');
     $mail2 = new PHPMailer;
     $mail2->CharSet="UTF-8";
     $mail2->setFrom($email);
-    $mail2->addReplyTo($email);
     $mail2->addAddress($email);
     $mail2->Subject = 'GOServer Informacion';
     $mail2->msgHTML($mensaje2);
     $mail2->AltBody = 'This is a plain-text message body';
-
+    if (!$mail2->send()) { } else {}
 }
 ?>
