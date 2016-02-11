6fc6c40ce473c5da41686e90170b73e3dde5fd2d
alumno <alumno>
2016-02-11 13:29:45 +0100

    gulp parametro

--- gulp.sh
+++ gulp.sh
@@ -1,4 +1,4 @@
-.\node\bin\node.exe .\node_modules\gulp\bin\gulp.js cliente1
-.\node\bin\node.exe .\node_modules\gulp\bin\gulp.js cliente2
-.\node\bin\node.exe .\node_modules\gulp\bin\gulp.js administrador1
-.\node\bin\node.exe .\node_modules\gulp\bin\gulp.js administrador2
+.\node\bin\node --max_old_space_size=300 .\node_modules\gulp\bin\gulp.js cliente1
+.\node\bin\node --max_old_space_size=300 .\node_modules\gulp\bin\gulp.js cliente2
+.\node\bin\node --max_old_space_size=300 .\node_modules\gulp\bin\gulp.js administrador1
+.\node\bin\node --max_old_space_size=300 .\node_modules\gulp\bin\gulp.js administrador2
