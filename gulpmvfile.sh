8955533ac68e8c3d80e1319799d93954a393af44
OpenShift System User <56b3375c2d527133e6000207@ex-std-node731.prod.rhcloud.com>
2016-02-15 12:24:28 -0500

    build

--- gulpmvfile.sh
+++ gulpmvfile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-rm -f ./go-server-openshift/*.php
-rm -f ./go-server-openshift/*.html
-rm -f ./go-server-openshift/js/*
-rm -f ./go-server-openshift/administrador/*.php
-rm -f ./go-server-openshift/administrador/*.html
-rm -f ./go-server-openshift/administrador/js/*
-cp -a ./build/web/* ./go-server-openshift/
+rm -f /tmp/go-server-openshift/daniel/tienda/*.php
+rm -f /tmp/go-server-openshift/daniel/tienda/*.html
+rm -f /tmp/go-server-openshift/daniel/tienda/js/*
+rm -f /tmp/go-server-openshift/daniel/tienda//administrador/*.php
+rm -f /tmp/go-server-openshift/daniel/tienda/administrador/*.html
+rm -f /tmp/go-server-openshift/daniel/tienda/administrador/js/*
+cp -a ./build/web/* /tmp/go-server-openshift/daniel/tienda/
